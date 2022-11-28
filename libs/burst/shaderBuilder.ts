d9bd6b5038a15c91d23386ec039cb371acd58c3a
Nathan Franck <nathanfranck1@gmail.com>
2022-11-28 01:13:58 -0700

    More naming experiments, maybe a little more concise / dignified?

--- libs/burst/shaderBuilder.ts
+++ libs/burst/shaderBuilder.ts
@@ -12,31 +12,31 @@ const typeBlueprints = <const>{
 type TypeBlueprints = typeof typeBlueprints;
 type Types = keyof typeof typeBlueprints;
 
-class Statement<T extends Types> {
+class Expr<T extends Types> {
     constructor(public type: T, public code: string) { }
-    add(other: Statement<T>): Statement<T> {
-        return new Statement(this.type, `(${this.code}) + ${other.code}`);
+    add(other: Expr<T>): Expr<T> {
+        return new Expr(this.type, `(${this.code}) + ${other.code}`);
     }
-    sub(other: Statement<T>): Statement<T> {
-        return new Statement(this.type, `(${this.code}) - ${other.code}`);
+    sub(other: Expr<T>): Expr<T> {
+        return new Expr(this.type, `(${this.code}) - ${other.code}`);
     }
-    mul(other: Statement<T>): Statement<T> {
-        return new Statement(this.type, `(${this.code}) * ${other.code}`);
+    mul(other: Expr<T>): Expr<T> {
+        return new Expr(this.type, `(${this.code}) * ${other.code}`);
     }
-    div(other: Statement<T>): Statement<T> {
-        return new Statement(this.type, `(${this.code}) / ${other.code}`);
+    div(other: Expr<T>): Expr<T> {
+        return new Expr(this.type, `(${this.code}) / ${other.code}`);
     }
-    neg(): Statement<T> {
-        return new Statement(this.type, `-(${this.code})`);
+    neg(): Expr<T> {
+        return new Expr(this.type, `-(${this.code})`);
     }
-    combine<U extends Types>(other: Statement<U>) {
+    combine<U extends Types>(other: Expr<U>) {
         return new Combiner(typeBlueprints[this.type], this.code).combine(other);
     }
     aggregate<U>(
         elements: readonly U[],
-        func: (previous: Statement<T>, elem: U, index: number) => Statement<T>
-    ): Statement<T> {
-        var statement = this as Statement<T>;
+        func: (previous: Expr<T>, elem: U, index: number) => Expr<T>
+    ): Expr<T> {
+        var statement = this as Expr<T>;
         for (let i = 0; i < elements.length; i++) {
             statement = func(statement, elements[i], i);
         }
@@ -46,58 +46,66 @@ class Statement<T extends Types> {
 
 class Combiner<Blueprint extends readonly number[]> {
     constructor(private blueprint: Blueprint, public code: string) { }
-    combine<U extends Types>(other: Statement<U>) {
+    combine<U extends Types>(other: Expr<U>) {
         return new Combiner(<const>[...this.blueprint, ...typeBlueprints[other.type]], `${this.code}, ${other.code}`);
     }
-    as<U extends keyof { [key in Types as TypeBlueprints[key] extends Blueprint ? key : never]: true }>(type: U): Statement<U> {
-        return new Statement(type, `${type}(${this.code})`);
+    as<U extends keyof { [key in Types as TypeBlueprints[key] extends Blueprint ? key : never]: true }>(type: U): Expr<U> {
+        return new Expr(type, `${type}(${this.code})`);
     }
 }
 
-class Constructor<T extends Types> extends Statement<T> {
-    constructor(type: T, ...args: ConvertTuple<TypeBlueprints[T], Statement<'float'>>) {
+class Value<T extends Types> extends Expr<T> {
+    constructor(type: T, ...args: ConvertTuple<TypeBlueprints[T], Expr<'float'>>) {
         super(type, `${type}(${args.map(arg => arg.code).join(', ')})`);
     }
 }
 
-class Literal<T extends Types> extends Statement<T> {
+class Const<T extends Types> extends Expr<T> {
     constructor(
         type: T,
         ...values: ConvertTuple<TypeBlueprints[T], number>
     ) {
         if (values.length == 1) {
-            super(type, values[0].toString());
+            super(type, Const.numberToFloatLiteral(values[0]));
         }
         else {
-            super(type, `${type}(${values.map(value => value.toString()).join(', ')})`);
+            super(type, `${type}(${values.map(value => Const.numberToFloatLiteral(value)).join(', ')})`);
         }
+    }
+
+    static numberToFloatLiteral(value: number) {
+        var string = value.toString();
+        if (string.indexOf('.') == -1) {
+            string += '.0';
+        }
+        return string;
     }
 }
 
-class ImperiativeBlock<Scope extends Record<string, any>> {
-    constructor(public code: string, public scope: Scope) { }
-    define<T extends Record<string, Statement<Types>>>(
+class CodeBlock<Scope extends Record<string, any> = {}> {
+    constructor(public code: string = '', public scope: Scope = {} as Scope) { }
+    define<T extends Record<string, Expr<Types>>>(
         func: (scope: { [key in keyof Scope]: Scope[key] }) => T
-    ): EvaluationCheck<ImperiativeBlock<Scope & T>, 'Scope collision on previous define for', keyof Scope & keyof T> {
+    ): EvaluationCheck<CodeBlock<Scope & T>, 'Scope collision on previous define for', keyof Scope & keyof T> {
         const statements = func(this.scope);
         const entries = toEntries(statements);
         const newScope = <const>{
             ...this.scope,
             ...fromEntries(entries.map(([key, value]) =>
-                [key, new Statement(value.type, key as string)]))
+                [key, new Expr(value.type, key as string)]))
         };
         const newCode = entries.reduce((code, [name, statement]) => {
             return `${code}
             ${statement.type} ${name as any} = ${statement.code};`;
         }, this.code);
-        return new ImperiativeBlock(newCode, newScope as any) as any;
+        return new CodeBlock(newCode, newScope as any) as any;
     }
 }
 
 // Living example.
 
 const floatConstants = {
-    one: 1.0,
+    onePointFive: 1.5,
     two: 2.0,
 }
 
@@ -108,27 +116,25 @@ const searchDirections: [number, number][] = [
     [0, -1],
 ];
 
-console.log(new ImperiativeBlock('', {})
-    .define(_ => mapObject(floatConstants, value => new Literal('float', value)))
+console.log(new CodeBlock()
+    .define(_ => mapObject(floatConstants, value => new Const('float', value)))
     .define($ => ({
-        first: $.one.add($.two),
-        second: $.one.sub($.two),
+        first: $.onePointFive.add($.two),
+        second: $.onePointFive.sub($.two),
     }))
     .define($ => ({
-        // a: $.b.add($.b).mul($.asdf).neg(),
-        // b: $.b.add($.b).mul($.asdf).neg(),
-        lotsaMaths: $.second.add($.second).mul($.one).neg(),
+        lotsaMaths: $.second.add($.second).mul($.onePointFive).neg(),
     }))
     .define($ => ({
-        aggregationExample: new Literal('vec2', 0, 0)
+        aggregationExample: new Const('vec2', 0, 0)
             .aggregate(searchDirections, (previous, direction) =>
-                previous.mul(new Literal('vec2', ...direction))),
-        vec3Example: new Constructor('vec3', $.first, $.second, new Literal('float', 0)),
-        mat2Example: new Constructor('mat2', $.first, $.second, $.one, $.lotsaMaths),
+                previous.mul(new Const('vec2', ...direction))),
+        vec3Example: new Value('vec3', $.first, $.second, new Const('float', 0)),
+        mat2Example: new Value('mat2', $.first, $.second, $.onePointFive, $.lotsaMaths),
     }))
     .define($ => ({
         result: $.aggregationExample.combine($.first).as('vec3')
-            .add($.vec3Example).combine($.one).as('mat2')
+            .add($.vec3Example).combine($.onePointFive).as('mat2')
             .add($.mat2Example),
     }))
     .code);
